28b972ebfb1fbf965cfe00683229615e730e1a84
Alex <alexanderpusey@gmail.com>
2023-03-15 23:07:25 -0600

    ica9

---
+++ ica/ica9/ica9.js
@@ -0,0 +1,11 @@
+const bttnone = document.getElementById('colorbttn')
+const text = document.getElementById('text')
+
+bttnone.addEventListener('click', function () {
+    bttnone.style.backgroundColor = "blue"
+})
+
+text.addEventListener('click', function () {
+    text.style.fontSize = '200px';
+})
+
